4989770d3220953c2e60c5a40340b506b6ad7c0c
damlaSevinc <sevincdamla@gmail.com>
2024-12-07 22:56:46 +0100

    Add UrlValidator dependency and isValidUrl method

--- src/main/java/com/example/url_shortener/UrlService.java
+++ src/main/java/com/example/url_shortener/UrlService.java
@@ -1,15 +1,26 @@
 package com.example.url_shortener;
 
+import org.apache.commons.validator.routines.UrlValidator;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class UrlService {
 
     public String shortenLongUrl(String longUrl){
+        if(!isValidUrl(longUrl)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid URL format");
+        }
         return "shortUrl";
     }
 
     public String getOriginalUrl(String shortUrl){
         return "originalUrl";
+    }
+
+    public boolean isValidUrl(String url){
+        UrlValidator urlValidator = new UrlValidator();
+        return urlValidator.isValid(url);
     }
 }
